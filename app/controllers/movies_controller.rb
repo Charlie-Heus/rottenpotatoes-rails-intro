a89dad902791c30d9e49cce1c5fba66bfdfb4b83
ch3593 Charlie Heus <ch3593@columbia.edu>
2023-10-05 21:57:09 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,18 +8,54 @@ class MoviesController < ApplicationController
 
   def index
 		@all_ratings = Movie.all_ratings
-		@ratings_to_show = []
+		@ratings_to_show = params[:ratings].present? ? params[:ratings].keys : @all_ratings
+		# sorted = params[:sorted]
+    session[:ratings] = @ratings_to_show
 
-		if params[:ratings] 
-			@ratings_to_show = params[:ratings].keys()
-		end
+    if params[:sorted].present? && @ratings_to_show.present?
+      @movies = Movie.where(rating: @ratings_to_show).order(params[:sorted])
+    elsif @ratings_to_show.present?
+      @movies = Movie.where(rating: @ratings_to_show)
+    else
+      @movies = []
+    end 
 
-		if @ratings_to_show.present?
-			@movies = Movie.where(rating: @ratings_to_show)
-		else
-			@movies = Movie.all
-		end
+
+		# if params[:ratings] 
+			# @ratings_to_show = params[:ratings].keys()
+
+		# elsif
+			# @ratings_to_show = session[:ratings]
+		# else
+			# @ratings_to_show = @all_ratings
+
+		# end
+
+		# sorted = params[:sorted]
+
+		# if @ratings_to_show.present?
+			# @movies = Movie.where(rating: @ratings_to_show).order(sorted)
+		# else
+      # @movies = []
+			# @movies = Movie.all.order(sorted)
+		# end
+
+		# if @ratings_to_show.present?
+			# @movies = Movie.where(rating: @ratings_to_show).order(@sorted_column)
+		# else
+			# @movies = Movie.all
+		# end
+
+    # Debugging output 
+    # puts "params[:sorted]: #{params[:sorted]}"
+    # puts "@sorted_column: #{@sorted_column}"
+    # puts "SQL Query: #{@movies.to_sql}"
+
   end
+
+  # def set_sorted_column
+    # @sorted_column = params[:sorted] if params[:sorted].present?
+  # end
 
   def new
     # default: render 'new' template
